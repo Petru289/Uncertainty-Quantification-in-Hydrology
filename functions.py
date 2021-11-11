fdae108b30d985222d0786bbc4165d795863d944
ga87poc <ga87poc@mytum.de>
2021-11-11 19:30:06 +0100

    Restructured code, added units to axis labels

---
+++ functions.py
@@ -0,0 +1,15 @@
+import numpy as np
+import chaospy as cp
+
+# Get the concentration
+def c(x, t, M, n, D, q, Lambda):
+    '''Returns the concentration at location x and time t with given porosity (n), dispersion coefficient (D), specific discharge (q) and decay rate (lambda)'''
+    c1 = M / np.sqrt(4 * np.pi * D * t)
+    c2 = np.exp(-(x - q * t / n) ** 2 / (4 * D * t))
+    c3 = np.exp(-Lambda * t)
+    return c1 * c2 * c3
+
+
+# Derivatives for the sensitivity analysis
+def dcdn(x, t, M, n, D, q, Lambda):
+    return c(x, t, M, n, D, q, Lambda) * q * (- x + q * t / n)/(2 * D * n ** 2)
---
+++ histogramm.py
@@ -0,0 +1,58 @@
+import numpy as np
+import chaospy as cp
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from functions import c
+
+# Load the parameters
+import parameters as par
+
+def histogramm():
+    plt.rc('xtick', labelsize=8) 
+    plt.rc('ytick', labelsize=8)
+
+    # Initialize variables
+    concentrations = np.zeros(shape = (len(par.x) * len(par.t), 100))
+
+    # Prepare the figure
+    fig, axes = plt.subplots(nrows = len(par.x), ncols = len(par.t))
+    plt.subplots_adjust(left=0.1, bottom = 0.1, right = 0.9, top = 0.9, wspace = 0.7, hspace = 0.8)
+    fig.suptitle('Histogramm of the $\it{concentration}$ for different $\it{x}$ and $\it{t}$', fontsize=16)
+
+    for distIndex, distance in enumerate(par.x): # Loop over all distances
+        for timeIndex, timestep in enumerate(par.t): # For all timesteps
+
+            # This is just an index to store all concentration values in a matrix
+            concentrationIndex = 3 * distIndex + timeIndex
+            
+            # Sample parameters from a joint distribution
+            n = cp.Uniform(par.nRange[0], par.nRange[1])
+            D = cp.Uniform(par.DRange[0], par.DRange[1])
+            q = cp.Uniform(par.qRange[0], par.qRange[1])
+            Lambda = cp.Uniform(par.LambdaRange[0], par.LambdaRange[1])
+            jointdist = cp.J(n, D, q, Lambda)
+            jointsample = jointdist.sample(size = 100)
+
+            # Calculate concentration
+            concentrations[concentrationIndex, :] = c(distance, timestep, par.M, jointsample[0,:], jointsample[1,:], jointsample[2,:], jointsample[3,:])
+            
+            # Statistical measures
+            mean = np.round(np.mean(concentrations[concentrationIndex, :]), 2)
+            std =  np.round(np.std(concentrations[concentrationIndex, :]), 2)
+            
+            # Plot subplots
+            axes[distIndex, timeIndex].hist(concentrations[concentrationIndex,:], 30)
+            axes[distIndex, timeIndex].set_title('x = {}, t = {}'.format(distance, timestep), fontsize = 8)
+            axes[distIndex, timeIndex].set_xlabel('concentration [$kg/m^3$]', fontsize = 8)
+            axes[distIndex, timeIndex].set_ylabel('Frequency', fontsize = 8)
+            textstring = '\n'.join((
+                '$\mu=%.2f$' % float(mean),
+                '$\sigma=%.2f$' % std
+            ))
+            axes[distIndex, timeIndex].text(0.75, 0.95, textstring, transform=axes[distIndex, timeIndex].transAxes, fontsize=8,
+            verticalalignment='top')
+
+    plt.show()
+
+if __name__ == "__main__":
+    histogramm()
