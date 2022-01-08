468197f0dd4271f8244012bbeb82ae6812dafffb
Petru289 <82600518+Petru289@users.noreply.github.com>
2022-01-08 20:14:55 +0100

    Dream algorithm fixed

--- dream_results.py
+++ dream_results.py
@@ -11,39 +11,41 @@ from spotpy.analyser import plot_posterior_parameter_histogram
 spot_setup = spotpy_setup()
 
 #M, d, k = 4, 2, 4 #https://spotpy.readthedocs.io/en/latest/Sensitivity_analysis_with_FAST/
-#rep = (1 + 4 * M ** 2 * (1 + (k - 2) * d)) * k
-rep = 1000
-sampler = spotpy.algorithms.fast(spot_setup,  dbname='FAST_hydrology',  dbformat='csv')
-sampler.sample(repetitions = rep)
+#rep = (1 + 4 * M ** 2 * (1 + (k - 2) *f d)) * k
+#sampler = spotpy.algorithms.fast(spot_setup,  dbname='FAST_hydrology',  dbformat='csv')
+#sampler.sample(repetitions = rep)
+#results_FAST = spotpy.analyser.load_csv_results('FAST_hydrology')
+#spotpy.analyser.plot_fast_sensitivity(results_FAST, number_of_sensitiv_pars=4)
 
-results_FAST = spotpy.analyser.load_csv_results('FAST_hydrology')
-spotpy.analyser.plot_fast_sensitivity(results_FAST, number_of_sensitiv_pars=4)
 
 r_hat = dream_run()
 results = spotpy.analyser.load_csv_results('HydrologyDREAM')
 fields = [word for word in results.dtype.names if word.startswith('sim')]
-print(results[fields])
 
-fig = plt.figure(figsize=(16,9))
-ax = plt.subplot(1,1,1)
-q5,q25,q75,q95=[],[],[],[]
-for field in fields:
-    q5.append(np.percentile(results[field][-100:-1],2.5))
-    q95.append(np.percentile(results[field][-100:-1],97.5))
-ax.plot(q5,color='dimgrey',linestyle='solid')
-ax.plot(q95,color='dimgrey',linestyle='solid')
-ax.fill_between(np.arange(0,len(q5),1),list(q5),list(q95),facecolor='dimgrey',zorder=0,
-                linewidth=0,label='parameter uncertainty')  
-ax.plot(spot_setup.evaluation(),'r.',label='data')
-ax.set_ylim(-5,70)
-ax.set_xlim(0,15)
-ax.legend()
-fig.savefig('python_hydrology.png',dpi=300)
+fig, axes = plt.subplots(nrows = 2, ncols = 1)
+for i in range(2):
+    Fields = fields[:7] if i == 0 else fields[7:]
+    q5, q25, q75, q95 = [], [], [], [] 
+    for field in Fields:
+        q5.append(np.percentile(results[field][-100:-1], 2.5))
+        q95.append(np.percentile(results[field][-100:-1], 97.5))
+    axes[i].plot(q5,color='dimgrey',linestyle='solid')
+    axes[i].plot(q95,color='dimgrey',linestyle='solid')
+    axes[i].fill_between(np.arange(0,len(q5),1),list(q5),list(q95),facecolor='dimgrey',zorder=0,
+                    linewidth=0,label='parameter uncertainty')
+    if i == 0:
+        axes[i].plot(spot_setup.evaluation()[:7],'r.', label='data')
+        axes[i].set_ylim(-0.3,40)
+    else:
+        axes[i].plot(spot_setup.evaluation()[7:],'r.', label='data')
+        axes[i].set_ylim(-0.3,15)
 
+    axes[i].set_xlim(0,6)   
+    axes[i].legend()
+    fig.savefig('python_hydrology.png', dpi = 300)               
+                   
 
 spotpy.analyser.plot_gelman_rubin(results, r_hat, fig_name='python_hydrology_convergence.png')
-
-
 parameters = spotpy.parameter.get_parameters_array(spot_setup)
 
 fig, ax = plt.subplots(nrows=4, ncols=2)
@@ -56,3 +58,54 @@ ax[-1][1].set_xlabel('Parameter range')
 
 plt.show()
 fig.savefig('hydrology_parameters.png',dpi=300)
+
+
+# SCRAP PAPER
+
+# def plotter(x):
+#     if x == 5:
+#         Fields = fields[:7]
+#     else:
+#         Fields = fields[7:]
+#     fig = plt.figure(figsize=(16,9))
+#     ax = plt.subplot(1,1,1)
+#     q5, q25, q75, q95 = [], [], [], [] 
+#     for field in Fields:
+#         q5.append(np.percentile(results[field][-100:-1], 2.5))
+#         q95.append(np.percentile(results[field][-100:-1], 97.5))
+#     ax.plot(q5,color='dimgrey',linestyle='solid')
+#     ax.plot(q95,color='dimgrey',linestyle='solid')
+#     ax.fill_between(np.arange(0,len(q5),1),list(q5),list(q95),facecolor='dimgrey',zorder=0,
+#                     linewidth=0,label='parameter uncertainty')
+#     if x == 5:
+#         ax.plot(spot_setup.evaluation()[:7],'r.', label='data')
+#         ax.set_ylim(-0.1,40)
+#         ax.set_xlim(0,7)
+#         ax.legend()
+#         fig.savefig('python_hydrology1.png', dpi = 300)
+#     else:
+#         ax.plot(spot_setup.evaluation()[7:],'r.', label='data')
+#         ax.set_ylim(-0.1,15)
+#         ax.set_xlim(0,6)
+#         ax.legend()
+#         fig.savefig('python_hydrology2.png', dpi = 300)
+
+# plotter(x = 5)
+# plotter(x = 50)
+
+
+# fig = plt.figure(figsize=(16,9))
+# ax = plt.subplot(1,1,1)
+# q5,q25,q75,q95=[],[],[],[]
+# for field in fields:
+#     q5.append(np.percentile(results[field][-100:-1],2.5))
+#     q95.append(np.percentile(results[field][-100:-1],97.5))
+# ax.plot(q5,color='dimgrey',linestyle='solid')
+# ax.plot(q95,color='dimgrey',linestyle='solid')
+# ax.fill_between(np.arange(0,len(q5),1),list(q5),list(q95),facecolor='dimgrey',zorder=0,
+#                 linewidth=0,label='parameter uncertainty')  
+# ax.plot(spot_setup.evaluation(),'r.',label='data')
+# ax.set_ylim(-5,70)
+# ax.set_xlim(0,15)
+# ax.legend()
+# fig.savefig('python_hydrology.png',dpi=300)
--- dream_setup.py
+++ dream_setup.py
@@ -3,22 +3,25 @@ import numpy as np
 import parameters as par
 from functions import c
 import obj
+from spotpy.likelihoods import gaussianLikelihoodMeasErrorOut as GausianLike
 
 #See documentation: https://spotpy.readthedocs.io/en/latest/Rosenbrock/
 
-#Inspired from spotpy.spot_setupdtlz1.py
+#Inspired from spotpy.spot_setupdtlz1.py. The issue is that the model (our concentration function) changes with the observations.
+#The following code is inspired from spot_setupdtlz1.py. Not sure if it's the correct approach; paramaters don't seem to converge to a distribution
 
 n_obs = 14 
-t = np.array([5, 10, 15, 20, 25, 30, 35])
+time = np.array([5, 10, 15, 20, 25, 30, 35])
+space = np.array([5, 50])
 
-def conc(x):
-    f = []
-    for i in range(0, n_obs):
-        if i <= 6:
-            f.append(c(5, t[i], 200, x[0], x[1], x[2], x[3]))
-        else:
-            f.append(c(50, t[i - 7], 200, x[0], x[1], x[2], x[3]))
-    return f
+# def conc(x):
+#     f = []
+#     for i in range(0, n_obs):
+#         if i <= 6:
+#             f.append(c(5, t[i], 200, x[0], x[1], x[2], x[3]))
+#         else:
+#             f.append(c(50, t[i - 7], 200, x[0], x[1], x[2], x[3]))
+#     return f
 
 
 class spotpy_setup(object):
@@ -32,20 +35,36 @@ class spotpy_setup(object):
         return spotpy.parameter.generate(self.params)
 
 
-    def simulation(self,vector):      
-            x = np.array(vector)
-            sim = conc(x)
-            return sim
+    # def simulation(self,vector):      
+    #         x = np.array(vector)
+    #         sim = conc(x)
+    #         return sim
 
+
+    # def evaluation(self):
+    #     observations = obj.obs[:,0].tolist() + obj.obs[:,1].tolist()
+    #     return observations
+
+
+    # def objectivefunction(self, simulation, evaluation):    
+    #     objective = []
+    #     for i,f in enumerate(simulation):
+    #         objective.append(GausianLike(data = [evaluation[i]], comparedata = [simulation[i]]))
+    #     return objective
+    
+    def simulation(self, x):
+        n, D, q, Lambda = x
+        sim = []
+        for s in range(len(space)):
+            for t in range(len(time)):
+                sim.append(c(space[s], time[t], 200, n, D, q, Lambda))
+        return sim
 
     def evaluation(self):
         observations = obj.obs[:,0].tolist() + obj.obs[:,1].tolist()
         return observations
 
-
-    def objectivefunction(self, simulation, evaluation):
-        objective = []
-        for i,f in enumerate(simulation):
-            objective.append(spotpy.objectivefunctions.mae(evaluation = [evaluation[i]], simulation = [simulation[i]]))
+    def objectivefunction(self, simulation, evaluation):    
+        objective = GausianLike(data = evaluation, comparedata = simulation)
         return objective
-    
+
