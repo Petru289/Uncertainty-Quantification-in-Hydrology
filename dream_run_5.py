46d92d0db2188f2ee8e7183722bb014b30077d38
Petru289 <82600518+Petru289@users.noreply.github.com>
2022-03-05 20:02:40 +0100

    update assignment 5

--- dream_run_5.py
+++ dream_run_5.py
@@ -19,12 +19,12 @@ def dream_run_5(new_meas):
     spot_setup = spotpy_setup(new_meas)
 
     #See spotpy.dream.py
-    rep = 6000
+    rep = 5000
     nChains = 5 #default is 5
     convergence_limit = 1.2 #default is 1.2
 
     nCr = 3 #default is 3
-    runs_after_convergence = 4000 #default is 100
+    runs_after_convergence = 20000 #default is 100
 
     sampler = spotpy.algorithms.dream(spot_setup, parallel=parallel, dbname='HydrologyDREAM_5', dbformat=dbformat, db_precision = np.float16, save_sim = True, sim_timeout=timeout)
     r_hat = sampler.sample(repetitions = rep,  nChains = nChains, convergence_limit = convergence_limit, runs_after_convergence = runs_after_convergence)
@@ -47,4 +47,4 @@ def dream_run_5(new_meas):
     #   * False: Simulation results will not be saved
 
 if __name__ == "main":
-    dream_run()
+    dream_run_5()
