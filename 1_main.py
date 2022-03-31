f83456a2c15e825dbeb14d51e71b6eba009c4780
ga87poc <ga87poc@mytum.de>
2022-03-31 20:05:53 +0200

    deleting old files

--- 1_main.py
+++
@@ -1,7 +0,0 @@
-from histogramm import histogramm
-from sensitivityAnalysis import sensitivityAnalysis
-
-histogramm()
-
-# The function expects the parameter you want to change as a string. Choose from: n, D, q, Lambda
-sensitivityAnalysis('n')
